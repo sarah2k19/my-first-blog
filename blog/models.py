1015f86ccfe85f4180c4196aff91cb2214551b0b
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-24 22:25:59 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -10,6 +17,15 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    #added after REST API implementation
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, null=True, blank=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
