13ef48edf69e4fbf6333b0c1a80ca1ac260272f4
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-25 22:05:17 +0100

    Update script.js

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function () {
-    $(document).click(function () {
-        $('div').fadeOut('fast');
-    });
+$( function() {
+    $( "div" ).sortable();
+    $( "div" ).disableSelection();
+    $( "h2" ).sortable({items: '> h2:not(.date)'});
+  } );
+
+
+$(document).ready(function(){
+
+  $('.post').hover(
+    function(){
+        $(this).fadeTo(500, 0.7);
+    },
+    function(){
+        $(this).fadeTo(500, 1.0);
+    }
+  );
 });
