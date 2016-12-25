fa4768c77fed6e3f111980262a96bc32c934c72b
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-25 22:05:57 +0100

    Update script.js

--- blog/static/css/js/script.js
+++ blog/static/css/js/script.js
@@ -1,36 +1,9 @@
-/*$(document).ready(function () {
-    $(document).click(function () {
-        $('div').fadeOut('fast');
-    });
-});
-
-
-$( function() {
-    $( "post" ).sortable();
-    $( "post" ).disableSelection();
-  } );
-*/
-
-
 $( function() {
     $( "div" ).sortable();
     $( "div" ).disableSelection();
     $( "h2" ).sortable({items: '> h2:not(.date)'});
   } );
 
-/*
-$(document).ready(function(){
-
-  $('.post').hover(
-    function(){
-        $(this).css('background-color', '#ffebe6');
-    },
-    function(){
-        $(this).css('background-color', '#ffe6c1');
-    }
-  );
-});
-*/
 
 $(document).ready(function(){
 
