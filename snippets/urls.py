3dac1264088bbb981284e092dc0c7c1cc03ca6ba
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-27 02:23:58 +0100

    Update urls.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -31,8 +31,8 @@ urlpatterns = format_suffix_patterns([
     url(r'^api/$', api_root, name='api-root'),
     url(r'^snippets/$', snippet_list, name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^authors/$', user_list, name='user-list'),
+    url(r'^authors/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 ])
 
 # Login and logout views for the browsable API
